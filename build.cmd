f59705b7f4ba09ecf0d549ead27a7e2e410d6b17
Steve Molloy <smolloy@microsoft.com>
2023-01-20 09:53:36 -0800

    Updates for build pipeline update (#11)
    * Lots of build updates to catch up to the last 5 years.
    
    * Better 'clean'; Update to correct Authenticode

--- build.cmd
+++ build.cmd
@@ -1,12 +1,18 @@
 @ECHO OFF
 
 setlocal
+set EnableNuGetPackageRestore=true
 
-set logOptions=/flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
+set MSBUILDEXE=msbuild.exe
 
-echo Please build from VS 2015(or newer version) Developer Command Prompt
+set cfgOption=/p:Configuration=Release
+REM set cfgOption=/p:Configuration=Debug
+REM set cfgOption=/p:Configuration=Debug;Release
+if not "%1"=="" set cfgOption=/p:Configuration=
 
-:BUILD
-msbuild "%~dp0MicrosoftAspNetOutputCache.msbuild" %logOptions% /v:m /maxcpucount /nodeReuse:false %*
+set logOptions=/v:n /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
+REM set logOptions=/v:diag /flp:Summary;Verbosity=diag;LogFile=msbuild.log /flp1:warningsonly;logfile=msbuild.wrn /flp2:errorsonly;logfile=msbuild.err
+
+%MSBUILDEXE% "%~dp0\MicrosoftAspNetOutputCache.msbuild" /t:Build %logOptions% /maxcpucount /nodeReuse:false %cfgOption%%*
 
 endlocal
